eeb89ffbd6a929df1afb17ce9b75f81ba3a86656
sunjin <changido2888@gmail.com>
2023-01-02 19:13:00 +0900

    pre

---
+++ python/실습.py
@@ -0,0 +1,62 @@
+# A = int(input("숫자를 입력해주세요 > "))
+# print(A + 10)
+
+# B = str(input("좋아하는 음식을 입력해주세요 > "))
+# print("좋아하는음식 :", B)
+
+# current = 2023
+# name = str(input("이름을 입력해주세요 >"))
+# year = int(input("태어난 년도를 입력해주세요 >")) 
+
+# print('저의 이름은' , name , '이고, 올해 나이는' , (current - year+1) , '세 입니다.' )
+
+# A = str(input('첫 번째 문장을 입력해주세요 >'))
+# B = str(input('두 번째 문장을 입력해주세요 > '))
+
+# print(A+B)
+
+# number = int(input('정수형 숫자를 입력해주세요 >'))
+
+# print(- number)
+
+# number1 = int(input('첫 번째 정수형 숫자를 입력해주세요 >'))
+# number2 = int(input('두 번째 정수형 숫자를 입력해주세요 >'))
+
+# print("더하기 연산", number1+number2)
+# print("빼기 연산", number1-number2)
+# print("곱하기 연산", number1*number2)
+# print("몫 연산", number1//number2)
+# print("나머지 연산", number1%number2)
+
+# a = int(input("첫 번째 정수형 숫자를 입력해주세요 >"))
+# b = int(input("두 번째 정수형 숫자를 입력해주세요 >"))
+# c = int(input("세 번째 정수형 숫자를 입력해주세요 >"))
+
+# print(round((a+b+c)/3))
+
+# a = int(input("첫 번째 정수형 숫자를 입력해주세요 >"))
+# b = int(input("두 번째 정수형 숫자를 입력해주세요 >"))
+# c = int(input("세 번째 정수형 숫자를 입력해주세요 >"))
+# d = int(input("네 번째 정수형 숫자를 입력해주세요 >"))
+# e = int(input("다섯 번째 정수형 숫자를 입력해주세요 >"))
+
+# print([a,b,c,d,e])
+
+# a = str(input("문자열을 입력해주세요 >"))
+# b = int(input("정수형 숫자를 입력해주세요 >"))
+
+# print(a*b)
+
+# a = int(input("첫 번째 정수형 숫자를 입력해주세요 >"))
+# print(a)
+# b = int(input("두 번째 정수형 숫자를 입력해주세요 >"))
+# print(a+b)
+# c = int(input("세 번째 정수형 숫자를 입력해주세요 >"))
+# print((a+b)+c)
+# d = int(input("네 번째 정수형 숫자를 입력해주세요 >"))
+# print((a+b+c)+d)
+# e = int(input("다섯 번째 정수형 숫자를 입력해주세요 >"))
+# print((a+b+c+d)+e)
+
+
+
