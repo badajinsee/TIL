8389904f4a3e7c946d2c5042f0f3f0fef1d9a11c
sunjin <changido2888@gmail.com>
2023-01-03 23:46:52 +0900

    upgrade2

---
+++ python/실습_제어문.py
@@ -0,0 +1,95 @@
+# 정수 한 개를 입력 받고, 해당 숫자가 0보다 큰 숫자라면 True 아니면 False를 출력하세요.
+
+# a = int(input('정수를 입력하세요 >'))
+
+# if a > 0 :
+#     print('True')
+# else :
+#     print('False')
+
+# 정수 두 개를 입력 받고, 크기가 더 큰 정수를 출력하세요.
+
+# 두 정수가 같으면 False를 출력하세요.
+
+# a = int(input('첫번째 정수를 입력하세요 >'))
+# b = int(input('두번째 정수를 입력하세요 >'))
+
+# if a > b :
+#     print(a)
+# elif b > a :
+#     print(b)
+# elif a == b :
+#     print("False")
+
+# 정수 한 개를 입력 받고, 해당 정수가 1 보다 크고, 10보다 작다면 True 아니면 False를 출력하세요.
+
+# a = int(input('정수를 입력하세요 >'))
+
+# if 1 < a and a < 10 :
+#     print('True')
+# else :
+#     print('False')
+
+# 정수 한 개를 입력 받고 0 보다 크고, 짝수라면 True 아니면 False를 출력하세요.
+
+# a = int(input('정수를 입력하세요 >'))
+
+# if a > 0 and (a%2==0):
+#     print('True')
+# else :
+#     print('False')
+
+# 정수 한 개를 입력 받고, 아래 조건에 따라 출력하세요.
+
+# 값이 100 초과 / 0 미만이면 "에러" 출력
+# 값이 60 이상이면 "합격" 출력
+# 값이 60 미만이면 "불합격" 출력
+
+# a = int(input('정수를 입력하세요 >'))
+
+# if a > 100 and a < 0 :
+#     print('에러')
+# elif a >= 60:
+#     print('합격')
+# else:
+#     print('불합격')
+
+# 문자열을 입력 받고, 입력 받은 문자열을 반대로 한 글자씩 출력하세요.
+
+# a = input('문자열을 입력하세요 >')
+
+# for char in a [::-1]:
+#     print(char)
+
+# 정수 두 개를 입력 받고, 두 수 사이의 정수를 오름차순으로 출력하세요.
+# 두 값이 같으면 False를 출력하세요
+
+a = int(input('첫번째 정수를 입력하세요 >'))
+b = int(input('두번째 정수를 입력하세요 >'))
+
+if a==b:
+    print("False")
+elif a > b:
+    for i in range(a-1,b,-1):
+        print(i, end=(' '))
+elif a < b :
+    for j in range(b-1,a,-1):
+        print(j, end=(' '))
+
+# 정수 한 개를 입력 받고, 1 부터 입력 값 사이의 정수 중 홀수만 출력하세요.
+# 입력 값이 1보다 작으면 False를 출력하세요.
+
+# a = int(input('정수를 입력하세요 >'))
+
+# if a>1 :
+#     for i in range(1,a,2):
+#         print(i)
+
+# else :
+#     print("False")
+
+# 구구단을 출력하세요.
+
+# for i in range(2,10):
+#     for j in range(1,10):
+#         print(f'{i} X {j} = {i*j}')
---
+++ python/예제_제어문.py
@@ -0,0 +1,142 @@
+# a = 1
+# b = 1
+
+# print(a < b) #false
+
+# a = bool("")
+# b = False
+# print(a == b) #true
+
+# a = 1
+# result = ""
+
+# if a == 1:
+#     result = True
+# else:
+#     result = False
+
+# print(result) #True
+
+# a = 90
+
+# if a == 90:
+#     a = a + 10
+
+# elif a == 100:
+#     a = a + 10
+
+# elif a == 110:
+#     a = a + 10
+
+# else:
+#     a = a + 10
+
+# a = a + 10 
+
+# print(a) #110
+
+# string = "hello world!"
+
+# for element in string: 
+#     print(element)
+
+"""
+h
+e
+l
+l
+o
+
+w
+o
+r
+l
+d
+!
+"""
+
+# list_variable = [0, 1, 2, 3, 4, 5, 6]
+
+# for element in list_variable:
+#     print(element, end=" ")
+
+# """
+# 0 1 2 3 4 5 6 
+# """
+
+# n = 10 
+
+# for element in range(-n, n):
+#     print(element, end=" ")
+
+# """
+# -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9
+"""
+
+# n = 10 
+
+# for element in range(1, n + 1, 3):
+#     print(element, end=" ")
+
+# """
+# 1 4 7 10
+# """
+
+# list_variable = [6, 5, 4, 3, 2, 1, 0]
+
+# for index, element in enumerate(list_variable):
+#     print(index, element)
+
+# """
+# 0 6
+# 1 5
+# 2 4 
+# 3 3 
+# 4 2 
+# 5 1
+# 6 0
+# """
+
+# n = 10
+
+# for element in range(n, -n, -1):
+#     print(element, end= " ")
+#     if n < 0 :
+#         break
+
+#     """
+#     10 9 8 7 6 5 4 3 2 1 0 
+    # """
+
+# list_variable = [-1, 3, 5, -2, 1, 9, 21, -3, -5]
+
+# for element in list_variable:
+#     if element < 0:
+#         continue
+# print(element, end=" ")
+
+# '''
+# -5
+# '''
+
+# N = 3
+# M = 4
+
+# for n in range(N):
+#     for m in range(M):
+#         print(f"{n}, {m}")
+
+'''
+0, 0
+0, 1 
+0, 2
+0, 3 
+1, 0
+1, 1
+1, 2
+1, 3
+2, 0
+2, 1
+2, 2
+2, 3
+'''
